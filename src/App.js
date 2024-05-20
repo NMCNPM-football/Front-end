54e1c1e7ce3d52efbee0e4eba19d07353e9697bb
DaoDaiChi <21520646@gm.uit.edu.vn>
2024-05-20 14:57:19 +0700

    fixing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import TeamGrid from './Components/ClubInfo/TeamGrid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FootballSchedule from './Components/FootballSchedule';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
-//import LeagueTable from './Components/Ranking/LeagueTable';
-//import FootballSchedule from './Components/FootballSchedule';
+import LeagueTable from './Components/Ranking/LeagueTable';
+
 function App() {
   return (
-    <div>
-      <Header/>
-      {/* <FootballSchedule/> */}
-      {/* <LeagueTable /> */}
-      <TeamGrid />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<FootballSchedule />} />
+        <Route path="/league-table" element={<LeagueTable />} />
+        {/* Add other routes as needed */}
+      </Routes>
       <Footer />
-    </div>
-    
+    </Router>
   );
 }
 
