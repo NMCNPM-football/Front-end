cbffc2c6ae91c212247efcbe87c11721b02c2ed3
DaoDaiChi <21520646@gm.uit.edu.vn>
2024-05-20 22:09:19 +0700

    fixing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage/HomePage.jsx';
 import LeagueTable from './Components/Ranking/LeagueTable';
+import Header from './Components/HomePage/Header';
+import Footer from './Components/HomePage/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return (
-      <Router>
-      <HomePage/>
-      <Routes>
-        <Route path="/bang-xep-hang" element={<LeagueTable />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/bang-xep-hang" element={<LeagueTable />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
