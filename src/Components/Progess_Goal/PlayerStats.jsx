8085b1f9398665b8e6035632cea723c261cb37cf
TobiraKhoa Senju <kiraisme182003@gmail.com>
2024-05-21 18:15:14 +0700

    clean code

--- src/Components/Progess_Goal/PlayerStats.jsx
+++ src/Components/Progess_Goal/PlayerStats.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 const goalTypes = {
   LBT01: 'Ghi bàn trực tiếp',
   LBT02: 'Đá phạt',
   LBT03: 'Phản lưới'
 };
+
 const PlayerStats = ({ match }) => {
   if (!match) return null;
 
@@ -17,7 +20,7 @@ const PlayerStats = ({ match }) => {
               <div className="player-info1">
                 [{player.number}] {player.name}
               </div>
-              <div className="goal-time1">{player.goalTime}'</div>
+              <div className="goal-time1">{player.goalTime}&apos;</div>
               <div className="goal-type1">{goalTypes[player.goalType]}</div>
             </li>
           ))}
@@ -31,7 +34,7 @@ const PlayerStats = ({ match }) => {
               <div className="player-info2">
                 [{player.number}] {player.name}
               </div>
-              <div className="goal-time2">{player.goalTime}'</div>
+              <div className="goal-time2">{player.goalTime}&apos;</div>
               <div className="goal-type2">{goalTypes[player.goalType]}</div>
             </li>
           ))}
@@ -41,4 +44,31 @@ const PlayerStats = ({ match }) => {
   );
 };
 
-export default PlayerStats;
+PlayerStats.propTypes = {
+  match: PropTypes.shape({
+    teamOne: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      players: PropTypes.arrayOf(
+        PropTypes.shape({
+          number: PropTypes.number.isRequired,
+          name: PropTypes.string.isRequired,
+          goalTime: PropTypes.number.isRequired,
+          goalType: PropTypes.oneOf(Object.keys(goalTypes)).isRequired
+        })
+      ).isRequired
+    }).isRequired,
+    teamTwo: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      players: PropTypes.arrayOf(
+        PropTypes.shape({
+          number: PropTypes.number.isRequired,
+          name: PropTypes.string.isRequired,
+          goalTime: PropTypes.number.isRequired,
+          goalType: PropTypes.oneOf(Object.keys(goalTypes)).isRequired
+        })
+      ).isRequired
+    }).isRequired
+  }).isRequired
+};
+
+export default PlayerStats
