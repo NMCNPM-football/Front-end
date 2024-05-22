3611340b1dd558418385e6cff130123aa0698834
phat072 <phanphatlqd2003@gmail.com>
2024-05-22 14:38:49 +0700

    fix es

--- src/Components/ClubInfo/TeamGrid.jsx
+++ src/Components/ClubInfo/TeamGrid.jsx
@@ -1,8 +1,9 @@
+// src/Components/ClubInfo/TeamGrid.js
+
 import React, { useState, useEffect } from 'react';
 import { Clubdata2022, Clubdata2023 } from './Clubdata';
 import './TeamGrid.css';
 import SeasonSelector from './SeasonSelect';
-import { Link } from 'react-router-dom';
 
 const TeamGrid = () => {
   const [selectedSeason, setSelectedSeason] = useState("2023-2024");
@@ -27,12 +28,10 @@ const TeamGrid = () => {
       <SeasonSelector selectedSeason={selectedSeason} onSeasonChange={setSelectedSeason} />
       <div className="team-grid">
         {teams.map((team, index) => (
-          <Link key={index} to={`/team/${team.idteam}`} className="team-link"> {/* Thay đổi đường dẫn */}
-            <div className="team-card">
-              <img src={team.image} alt={team.clubName} />
-              <p>{team.clubName}</p>
-            </div>
-          </Link>
+          <div key={index} className="team-card">
+            <img src={team.image} alt={team.nameClub} />
+            <p>{team.nameClub}</p>
+          </div>
         ))}
       </div>
     </div>
