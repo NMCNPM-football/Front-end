2647171943b1512c49e2293b35c180877f99e7a0
phat072 <phanphatlqd2003@gmail.com>
2024-05-22 08:37:24 +0700

    add singup/in

---
+++ src/Components/SignIn/index.jsx
@@ -0,0 +1,109 @@
+import {NavBar} from "component/navbar";
+import {FormProvider, useForm} from 'react-hook-form';
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {PATHS} from "const";
+import UserService from "services/user"
+import {showError} from 'component/flash_message/flashMessageSlice';
+import {getProfile, setLoginSuccess} from "store/userSlice";
+import TextInput from "component/text_input";
+
+export default function SignIn() {
+  // const {t} = useTranslation();
+  const methods = useForm();
+  const navigate = useNavigate()
+  const isLogin = useSelector((state) => !!state.user.refreshToken)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLogin) {
+      navigate(PATHS.HOMEPAGE)
+    }
+  }, [isLogin, navigate])
+
+  const onSubmit = async (data) => {
+    try {
+      const result = await UserService.login(data)
+      dispatch(setLoginSuccess(result))
+      dispatch(getProfile())
+
+    } catch (error) {
+      const {message} = error
+      dispatch(showError({message}))
+    }
+  }
+
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8 ">
+        <div className="w-full max-w-sm space-y-10">
+          <div>
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <FormProvider {...methods}>
+            <form className="space-y-6" onSubmit={methods.handleSubmit(onSubmit)}>
+              <div className="relative -space-y-px rounded-md shadow-sm">
+                <div
+                  className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300"/>
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <TextInput
+
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <TextInput
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </FormProvider>
+          <p className="text-center text-sm leading-6 text-gray-500">
+            Not a member?{' '}
+            <Link to={PATHS.SIGN_UP} className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/lib/clicky/util/constants.js
+++ src/lib/clicky/util/constants.js
@@ -2,16 +2,7 @@ export const unsupportedKeyCharRegex = new RegExp('^\\s+|\\\.|\:|\\\$|\'|\"|\\\\
 export const unsupportedValueCharRegex = new RegExp("^\\s+|\'|\"|\\\\|\\s+$", 'g')
 export const NOTIFICATION_VIEWED = 'Notification Viewed'
 export const NOTIFICATION_CLICKED = 'Notification Clicked'
-export const CHARGED_ID = 'Charged ID'
-export const CHARGED_AMOUNT = 'Amount'
 export const CLIENT_ID = 'client_id'
-export const COOKIE_EXPIRY = 86400 * 365 // 1 Year in seconds
-export const CHARGED_ID_COOKIE_NAME = 'CHARGED_ID'
-export const EVT_PUSH = 'push'
-export const CUSTOMER_ID = 'Customer ID'
-export const CUSTOMER_EMAIL = 'Email'
-export const CUSTOMER_PHONE = 'Phone'
-export const UTM_VISITED = "UTM_VISITED"
 
 export const SYSTEM_EVENTS = [
     'Stayed',
@@ -27,5 +18,5 @@ export const DATA_NOT_SENT_TEXT = 'This property has been ignored.'
 export const CLICKY_ERROR_PREFIX = 'Clicky error:' // Formerly wzrk_error_txt
 export const EVENT_ERROR = `${CLICKY_ERROR_PREFIX} Event structure not valid. ${DATA_NOT_SENT_TEXT}`
 export const EMBED_ERROR = `${CLICKY_ERROR_PREFIX} Incorrect embed script.`
-export const TARGET_DOMAIN = "localhost:8088"
+export const TARGET_DOMAIN = "localhost:8888"
 export const TARGET_PROTOCOL = "http"
